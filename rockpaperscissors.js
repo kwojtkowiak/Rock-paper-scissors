d269c87b806fb4ff9eac67f754351d67d64e447d
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-20 23:09:37 +0200

    Add else statement to singleRound func

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 const picks = ["Rock","Paper","Scissors"];
 const computerSelection = computerPlay();
-const playerSelection = prompt("What do you choose? Rock, paper or scissors?")
+const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock")
 
 function computerPlay() {
     return picks[Math.floor(Math.random()*picks.length)];
@@ -8,4 +8,7 @@ function computerPlay() {
 
 function singleRound(playerSelection, computerSelection) {
     if 
+    else {
+        alert("Ayy lmao, it's not a valid option")
+    }
 }
