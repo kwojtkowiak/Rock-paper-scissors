f6929d9935e836283b4d77c01cf99f1b8bc0fad4
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-21 18:36:01 +0200

    Comment out unfinished func

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,24 +1,24 @@
 const picks = ["Rock","Paper","Scissors"];
 const computerSelection = computerPlay();
-const playerSelection = inputCheck(prompt("What do you choose? Rock, paper or scissors?", "Rock"));
+const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock");
 
 function inputCheck(playerSelection) {
 if (playerSelection.toLowerCase() == "rock" || "paper" || "scissors") {
     alert("Your choice has been made. Proceeding...");
   }
 else {
-    greeting = "That's not a valid choice!";
+    alert("That's not a valid choice!");
   }
-inputCheck(playerSelection)
+inputCheck();
 }
 
 function computerPlay() {
     return picks[Math.floor(Math.random()*picks.length)];
 }
 
-function singleRound(playerSelection, computerSelection) {
-    if 
-    else {
-        alert("Ayy lmao, it's not a valid option")
-    }
-}
+//function singleRound(playerSelection, computerSelection) {
+ //   if 
+  //  else {
+  //  alert("Ayy lmao, it's not a valid option")
+  //   } 
+  //  }
