3e360be5bac998f880fe89ef7e69e53cc130760f
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-22 00:01:04 +0200

    Add check

--- rockpaperscissorsv2.js
+++ rockpaperscissorsv2.js
@@ -1,10 +1,23 @@
 const picks = ["Rock","Paper","Scissors"];
 const computerSelection = computerPlay();
+console.log(computerSelection);
 const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock");
 
 function computerPlay() {
     return picks[Math.floor(Math.random()*picks.length)];
 }
+
+function inputCheck(playerSelection) {
+    let text = playerSelection.toLowerCase();
+    if (text === "rock" || text === "paper" || text === "scissors") {
+        alert("Your choice has been made. Proceeding...");
+      }
+    else {
+        alert("That's not a valid choice!");
+        text = prompt("What do you choose? Rock, paper or scissors?", "Rock");
+        inputCheck(text);
+      }
+    }
 
 function singleRound(playerSelection, computerSelection) {
 let input = playerSelection.toLowerCase();
@@ -25,3 +38,5 @@ else {
     return result = console.log("You win!")
 }
 }
+
+console.log(singleRound(playerSelection, computerSelection))
