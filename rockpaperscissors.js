72e2dfcfb75d8c80e7efee9ea35e1e288e097efa
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-21 23:00:19 +0200

    Remove endless loop in inputCheck, add new var in inputCheck

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,13 +3,13 @@ const computerSelection = computerPlay();
 const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock");
 
 function inputCheck(playerSelection) {
-if (playerSelection.toLowerCase() !== "rock" || "paper" || "scissors") {
+let input = playerSelection.toLowerCase();
+if (input === "rock" || input === "paper" || input === "scissors") {
     alert("Your choice has been made. Proceeding...");
   }
 else {
     alert("That's not a valid choice!");
   }
-inputCheck();
 }
 
 function computerPlay() {
