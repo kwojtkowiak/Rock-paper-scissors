a98b7762d9cf5188c667144c4341d185044c849c
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-21 16:38:13 +0200

    Add colon

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 const picks = ["Rock","Paper","Scissors"];
 const computerSelection = computerPlay();
-const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock")
+const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock");
 
 function computerPlay() {
     return picks[Math.floor(Math.random()*picks.length)];
