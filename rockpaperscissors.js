e9b9ef0cefafe08070bb34dc5995cd254e8302c8
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-06-21 18:23:15 +0200

    Add inputCheck func

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,6 +2,16 @@ const picks = ["Rock","Paper","Scissors"];
 const computerSelection = computerPlay();
 const playerSelection = prompt("What do you choose? Rock, paper or scissors?", "Rock");
 
+function inputCheck(playerSelection) {
+if (playerSelection.toLowerCase() == "rock" || "paper" || "scissors") {
+    alert("Your choice has been made. Proceeding...");
+  }
+else {
+    greeting = "That's not a valid choice!";
+    break
+  }
+}
+
 function computerPlay() {
     return picks[Math.floor(Math.random()*picks.length)];
 }
