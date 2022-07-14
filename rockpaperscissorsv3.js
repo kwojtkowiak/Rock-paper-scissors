e97d0b076ab73337fb93fb24a92f050b71fcbfe9
kwojtkowiak <80783985+kwojtkowiak@users.noreply.github.com>
2022-07-14 13:47:04 +0200

    Change singleround function to more optimized (reduce ifs)

--- rockpaperscissorsv3.js
+++ rockpaperscissorsv3.js
@@ -1,72 +1,44 @@
-//Some starting stuff
-const picks = ["Rock","Paper","Scissors"];
+//Starting list of possible choices for computer's choice
+//and starting counters
+const picks = ["ROCK","PAPER","SCISSORS"];
 const computerSelection = computerPlay();
-console.log(computerSelection);
-let winCounter = 0;
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ""
 
-//Alerting winner func
-function alertWinner() {
-    if (winCounter >= 3) {
-        return alert(`You've won ${winCounter} out of 5 games. You win!`);
-    } else if (winCounter === 2.5) {
-        return alert(`You've won 2.5 out of 5 games. It's a tie!`);
-    } else {
-        return alert(`You've won ${winCounter} out of 5 games. You lost!`);
-    }
-}
 
-//Computer move
-function computerPlay() {
-    return picks[Math.floor(Math.random()*picks.length)];
-}
 //Single round function
 function singleRound(playerSelection, computerSelection) {
-    let getPlayerInsensitive = playerSelection.toLowerCase();
-    if (getPlayerInsensitive === "rock" && computerSelection === "Rock") {
-      winCounter = winCounter+0.5;
-      return "It's a tie, both are Rock";
-    } else if (getPlayerInsensitive === "rock" && computerSelection === "Paper") {
-      winCounter = winCounter;
-      return "You Lose! Paper beats Rock";
-    } else if (getPlayerInsensitive === "rock" && computerSelection === "Scissors") {
-      winCounter = winCounter+1;
-      return "You win! Rock beats Scissors";
-    } else if (getPlayerInsensitive === "paper" && computerSelection === "Rock") {
-      winCounter = winCounter+1;
-      return "You win! Paper beats Rock";
-    } else if (getPlayerInsensitive === "paper" && computerSelection === "Paper") {
-        winCounter = winCounter+0.5;
-      return "It's a tie! Both are Paper";
-    } else if (getPlayerInsensitive === "paper" && computerSelection === "Scissors") {
-      winCounter = winCounter;
-      return "You lose! Scissors beats Paper";
-    } else if (getPlayerInsensitive === "scissors" && computerSelection === "Rock") {
-      winCounter = winCounter;
-      return "You lose! Rock beats Scissors";
-    } else if (getPlayerInsensitive === "scissors" && computerSelection === "Scissors") {
-        winCounter = winCounter+0.5;
-      return "It's a tie! Both are Scissors";
-    } else if (getPlayerInsensitive === "scissors" && computerSelection === "Paper") {
-      winCounter = winCounter+1;
-      return "You win! Scissors beat Paper";
-    } else {
-      return "Check your spelling!";
-    } 
+    if (playerSelection === computerSelection) {
+      roundWinner = "tie"
+    }
+    else if
+      ((playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+      (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+      (playerSelection === "SCISSORS" && computerSelection === "PAPER")) 
+      {
+        playerScore++
+        roundWinner = 'player'
+      }
+    else
+      ((computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+      (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+      (computerSelection === "SCISSORS" && playerSelection === "PAPER")) 
+      {
+        computerScore++
+        roundWinner = 'computer'
+      }
+      updateScoreMessage(roundWinner,playerSelection,computerSelection)
    }
+
+//Computer move "generator"
+function computerPlay() {
+  return picks[Math.floor(Math.random()*picks.length)];
+}
+
+//Checks if game's over
+function isGameOver() {
+  if (playerScore === 5 || computerScore === 5)
+}
     
-//Main game
-/* function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("What do you choose? Rock, paper or scissors?", "");
-        if (playerSelection === null || playerSelection === "") {
-        alert("You clicked Cancel!");
-        }
-        alert(singleRound(playerSelection, computerPlay()));
-        //outputs the winner of 5 games
-        if (i === 4) {
-        alert("You've played 5 games");}
-    }
-        alertWinner();
-    }
-   */
   console.log(game());
